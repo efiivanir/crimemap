5d614edb11d6bb1ef5cff588a2cd61eb08a30fb8
Ivanir Efi <efi.ivanir@gmail.com>
2022-07-27 12:52:44 +0300

    Complete chapter 8

--- crimemap.py
+++ crimemap.py
@@ -1,3 +1,7 @@
+import datetime
+import string
+
+import dateparser
 from dbhelper import DBHelper
 from flask import Flask
 import json
@@ -7,42 +11,51 @@ import os
 
 app = Flask(__name__)
 DB = DBHelper()
+categories = ['mugging', 'break-in']
+
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+def sanitize_string(userinput):
+    whitelist = string.ascii_letters + string.digits + " !?$.,;:-'()&"
+    clean = ''.join(list(filter(lambda x: x in whitelist, userinput)))
+    return clean
+
 
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes, code=os.environ.get('GOOGLE_MAPS_APY_KEY'))
+    return render_template("home.html", crimes=crimes,
+                           code=os.environ.get('GOOGLE_MAPS_APY_KEY'),
+                           categories=categories, error_message=error_message,
+                           )
 
 
-@app.route("/add", methods=["POST"])
-def add():
-    try:
-        data = request.form.get("userinput")
-        DB.add_input(data)
-    except Exception as e:
-        print(e)
-    return home()
-
-
-@app.route("/clear")
-def clear():
-    try:
-        DB.clear_all()
-    except Exception as e:
-        print(e)
-    return home()
 
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
     category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    description = request.form.get("description")
+    if category not in categories:
+        return home()
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format")
+    try:
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except ValueError:
+        return home()
+
+    description = sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True,host='0.0.0.0')
+    app.run(port=5002, debug=True,host='0.0.0.0')
--- dbhelper.py
+++ dbhelper.py
@@ -8,41 +8,7 @@ db_file = os.path.join(BASE_DIR, "crimemap.db")
 
 
 class DBHelper:
-    def get_all_inputs(self):
-        conn = None
-        try:
-            conn = sqlite3.connect(db_file)
-            # query = 'SELECT "description" FROM crimes;'
-            query = 'SELECT description FROM crimes;'
-            rows = list(conn.cursor().execute(query))
-            #rows = conn.cursor().fetchall()
-            return rows
-        except Error as e:
-            print(e)
-        finally:
-            conn.close()
 
-    def add_input(self, data):
-        try:
-            conn = sqlite3.connect(db_file)
-            query = "INSERT INTO crimes (description) VALUES('{}');".format(data)
-            conn.cursor().execute(query)
-            conn.commit()
-        except Error as e:
-            print(e)
-        finally:
-            conn.close()
-
-    def clear_all(self):
-        try:
-            conn = sqlite3.connect(db_file)
-            query = "DELETE FROM crimes;"
-            conn.cursor().execute(query)
-            conn.commit()
-        except Error as e:
-            print(e)
-        finally:
-            conn.close()
 
     def add_crime(self, category, date, latitude, longtitude,description):
         try:
